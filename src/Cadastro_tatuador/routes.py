32c9e833e9f6c4446142cd58f97882aa1f557470
Milena Guido <milenaetec2025@gmail.com>
2026-08-31 20:47:50 -0300

    correção routes.py

--- src/Cadastro_tatuador/routes.py
+++ src/Cadastro_tatuador/routes.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, render_template, redirect
 from forms import RegisterForm
 
-# Criamos um Blueprint chamado 'main'
 main_bp = Blueprint('main', __name__)
+
+# Rota raiz para redirecionar automaticamente e evitar o erro 404 ao abrir o servidor
+@main_bp.route('/')
+def home():
+    return redirect('/register')
 
 @main_bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        return redirect('/template')
+        return redirect('/template')  # Redireciona para a URL '/template'
     
     return render_template('register.html', form=form)
 
